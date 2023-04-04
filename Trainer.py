c79f11373f20f59055f26aa167c7278a9c34bc0f
Lewis JooHyeong Lee <jhhome3190@naver.com>
2023-04-05 00:03:50 +0900

    add: Trainer skeleton file

---
+++ Trainer.py
@@ -0,0 +1,25 @@
+class Trainer():
+    def __init__(self, ):
+        ## model 정의
+        
+        ## optimizer
+        
+        ## Loss
+        
+        ## 데이터
+        
+        ## 기타 하이퍼파라미터
+        
+        
+    def train(self, ):
+        for epoch in range ...:
+            for batch in Batch:
+                # 학습 데이터, 라벨
+                
+                # 모델 아웃풋
+                
+                # optim, loss
+                
+                # 출력 세팅
+                
+                # 프린팅 / loss 값 튀는 것 방지 옵션
